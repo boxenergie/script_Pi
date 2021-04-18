7de1218964f31196c0842007d9977ff5f0328306
bailletmanon <61618757+bailletmanon@users.noreply.github.com>
2021-04-18 13:45:50 +0200

    Update script to add settings file

--- script_install.sh
+++ script_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-curl -sL https://raw.githubusercontent.com/boxenergie/theme_dark_nodered/master/demo_v1 > flows_raspberrypi.json true
+curl -sL https://raw.githubusercontent.com/boxenergie/Script_Pi/main/flows_boxenergie.json > flows_raspberrypi.json true
 curl -sL https://raw.githubusercontent.com/boxenergie/Script_Pi/main/package.json > package.json true
+curl -sL https://raw.githubusercontent.com/boxenergie/Script_Pi/main/settings.js > settings.js true
 bash <(curl -sL https://raw.githubusercontent.com/boxenergie/Script_Pi/main/dashboard_install.sh)
